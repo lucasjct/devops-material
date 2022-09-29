3a7864fb9f3c89012eeeb21cbe085327654b1688
Lucas Teixeira <38733166+lucasjct@users.noreply.github.com>
2022-09-28 22:40:04 -0300

    script-bash

---
+++ apache.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install apache2 -y
+sudo apt-get install unzip
+
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+cp -R * /var/www/html/
